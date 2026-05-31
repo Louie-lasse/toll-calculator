35e5c0313c5cbd904acc855f287811e2ff79a5bb
Louie-lasse <love.lindqvist101@gmail.com>
2026-05-31 13:35:37 +0200

    Logic and formatting

--- Java/TollCalculator.java
+++ Java/TollCalculator.java
@@ -22,35 +22,46 @@ public class TollCalculator {
       long diffInMillies = date.getTime() - intervalStart.getTime();
       long minutes = timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS);
 
-      if (minutes <= 60) {
-        if (totalFee > 0) totalFee -= tempFee;
-        if (nextFee >= tempFee) tempFee = nextFee;
-        totalFee += tempFee;
-      } else {
+      if (minutes > 60) {
         totalFee += nextFee;
+        continue;
       }
+      if (totalFee > 0)
+        totalFee -= tempFee;
+      if (nextFee >= tempFee)
+        tempFee = nextFee;
+      totalFee += tempFee;
     }
-    if (totalFee > 60) totalFee = 60;
-    return totalFee;
+    return Math.min(totalFee, 60);
   }
 
   public int getTollFee(final Date date, Vehicle vehicle) {
-    if(isTollFreeDate(date) || vehicle.isTollFree()) return 0;
+    if (vehicle.isTollFree() || isTollFreeDate(date))
+      return 0;
     Calendar calendar = GregorianCalendar.getInstance();
     calendar.setTime(date);
     int hour = calendar.get(Calendar.HOUR_OF_DAY);
     int minute = calendar.get(Calendar.MINUTE);
 
-    if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-    else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-    else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-    else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-    else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-    else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-    else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-    else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-    else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-    else return 0;
+    if (hour == 6 && minute >= 0 && minute <= 29)
+      return 8;
+    if (hour == 6 && minute >= 30 && minute <= 59)
+      return 13;
+    if (hour == 7 && minute >= 0 && minute <= 59)
+      return 18;
+    if (hour == 8 && minute >= 0 && minute <= 29)
+      return 13;
+    if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59)
+      return 8;
+    if (hour == 15 && minute >= 0 && minute <= 29)
+      return 13;
+    if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59)
+      return 18;
+    if (hour == 17 && minute >= 0 && minute <= 59)
+      return 13;
+    if (hour == 18 && minute >= 0 && minute <= 29)
+      return 8;
+    return 0;
   }
 
   private Boolean isTollFreeDate(Date date) {
@@ -61,21 +72,22 @@ public class TollCalculator {
     int day = calendar.get(Calendar.DAY_OF_MONTH);
 
     int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-    if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) return true;
+    if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY)
+      return true;
 
-    if (year == 2013) {
-      if (month == Calendar.JANUARY && day == 1 ||
-          month == Calendar.MARCH && (day == 28 || day == 29) ||
-          month == Calendar.APRIL && (day == 1 || day == 30) ||
-          month == Calendar.MAY && (day == 1 || day == 8 || day == 9) ||
-          month == Calendar.JUNE && (day == 5 || day == 6 || day == 21) ||
-          month == Calendar.JULY ||
-          month == Calendar.NOVEMBER && day == 1 ||
-          month == Calendar.DECEMBER && (day == 24 || day == 25 || day == 26 || day == 31)) {
-        return true;
-      }
+    if (year != 2013) {
+      return false;
+    }
+    if (month == Calendar.JANUARY && day == 1 ||
+        month == Calendar.MARCH && (day == 28 || day == 29) ||
+        month == Calendar.APRIL && (day == 1 || day == 30) ||
+        month == Calendar.MAY && (day == 1 || day == 8 || day == 9) ||
+        month == Calendar.JUNE && (day == 5 || day == 6 || day == 21) ||
+        month == Calendar.JULY ||
+        month == Calendar.NOVEMBER && day == 1 ||
+        month == Calendar.DECEMBER && (day == 24 || day == 25 || day == 26 || day == 31)) {
+      return true;
     }
     return false;
   }
 }
-
